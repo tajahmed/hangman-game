e7639c16cf5c04fa032c84a4903b2c045958f132
tajahmed <m.t.a.78692@gmail.com>
2021-01-20 10:03:10 +0530

    deplonpm start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,105 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Header from './components/Header';
+import Figure from './components/Figure';
+import WrongLetters from './components/WrongLetters';
+import Word from './components/Word';
+import Popup from './components/Popup';
+import Notification from './components/Notification';
+import { showNotification as show, checkWin } from './helpers/helpers';
+
 import './App.css';
 
+const words = ['application', 'programming', 'interface', 'wizard'];
+// newword();
+let newentry;
+var addword = window.confirm("Would You like to add Your own words ?");
+if (addword){
+  newentry = prompt("Enter Your own words","");
+      alert (`${newentry} has been added to the random list`);
+      words.push(newentry);
+      console.log(words);
+} else {
+  alert("You be playing with pre-configuted words")
+}
+
+
+// function newword (){
+// let newentry;
+  
+//     alert('entered');
+//     // let word = "true";
+//     for (let i=0; i<2 ; i++){
+//       alert('entered for')
+//       console.log(i)
+//       if (newword.value.trim()===""){
+//         // word = "false";
+//         alert("All the Entered words added to list");
+//         alert("Let the game begin");
+//       }else{      
+      
+//     }
+//   }else {
+//   alert("You be playing with pre-configuted words")
+//   return
+//  }
+// }
+  
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
 function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [showNotification, setShowNotification] = useState(false);
+
+  useEffect(() => {
+    const handleKeydown = event => {
+      const { key, keyCode } = event;
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase();
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters(currentLetters => [...currentLetters, letter]);
+          } else {
+            show(setShowNotification);
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters(currentLetters => [...currentLetters, letter]);
+          } else {
+            show(setShowNotification);
+          }
+        }
+      }
+    }
+    window.addEventListener('keydown', handleKeydown);
+
+    return () => window.removeEventListener('keydown', handleKeydown);
+  }, [correctLetters, wrongLetters, playable]);
+
+  function playAgain() {
+    setPlayable(true);
+
+    // Empty Arrays
+    setCorrectLetters([]);
+    setWrongLetters([]);
+
+    const random = Math.floor(Math.random() * words.length);
+    selectedWord = words[random];
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="game-container">
+        <Figure wrongLetters={wrongLetters} />
+        <WrongLetters wrongLetters={wrongLetters} />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+      </div>
+      <Popup correctLetters={correctLetters} wrongLetters={wrongLetters} selectedWord={selectedWord} setPlayable={setPlayable} playAgain={playAgain} />
+      <Notification showNotification={showNotification} />
+    </>
   );
 }
 
